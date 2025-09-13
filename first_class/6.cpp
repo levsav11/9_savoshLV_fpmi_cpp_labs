c7a4355345e00f0aa14887bcbb5648c571d2f4a5
wiline <lev.savosh11@mail.ru>
2025-09-13 15:20:53 +0300

    added  6.cpp

---
+++ first_class/6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int n;int a=1;long s=0;
+    //Ввод и проверка
+    std::cout << "Введите n: ";
+    while (!(std::cin >> n) || n <= 0) {
+    std::cout << "n должно быть больше 0 и целым числом. Введите n: " << std::endl;
+    std::cin.clear();
+    std::cin.ignore(1000, '\n');}
+
+    while (a<=n){
+        s+=a;
+        a+=2;
+    }
+    std::cout << "Сумма первых n нечётных чисел: " << s;
+}
