567a86b462b00e53363e118ca6fa43d2f4e79cc7
wiline <lev.savosh11@mail.ru>
2025-09-14 12:53:09 +0300

    fixed 6.cpp

--- first_class/6.cpp
+++ first_class/6.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-int main() {
+int main(){
     setlocale(LC_ALL, "RUS");
     int n;int a=1;long s=0;
     //Ввод и проверка
@@ -8,10 +8,5 @@ int main() {
     std::cout << "n должно быть больше 0 и целым числом. Введите n: " << std::endl;
     std::cin.clear();
     std::cin.ignore(1000, '\n');}
-
-    while (a<=n){
-        s+=a;
-        a+=2;
-    }
-    std::cout << "Сумма первых n нечётных чисел: " << s;
-}
+    //Так как сумма первых n нечетных чисел равна квадрату n
+    std::cout << "Сумма первых n нечётных чисел: " << (n*n);}
