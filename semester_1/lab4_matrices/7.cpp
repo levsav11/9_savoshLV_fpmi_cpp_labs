795f54bd5644b28bd6141eaff1a26a390b90c958
wiline <lev.savosh11@mail.ru>
2025-10-15 12:28:49 +0300

    done 7.cpp

--- semester_1/lab4_matrices/7.cpp
+++ semester_1/lab4_matrices/7.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <iomanip>
 #include <limits>
-//Печать
+//Печать строки
 void print(std::string str){
     std::cout << str;
+}
+//Печать int
+void printint(int a){
+    std::cout << a;
 }
 //Печать массива
 void printArray(int* A,const int &len){
@@ -13,7 +17,7 @@ void printArray(int* A,const int &len){
     std::cout << std::endl;
 }
 
-std::string spaces(const int &count){
+std::string spaces(const int count){
     std::string spaces="";
     for (int i=1;i<=count;i++){
         spaces += " ";
@@ -21,20 +25,21 @@ std::string spaces(const int &count){
     return spaces;
 }
 
-void printMatrix(int** M,const int &rows, const int &cols,int maxwidth){
-    for (int j = 0; j < cols; j++){
-        for (int i = 0; i < rows; i++){
-            std::cout << M[i][j] << spaces(maxwidth-std::to_string(M[i][j]).length());
+void printMatrix(int** matr,int rows,int cols,int maxwidth) {
+    for (int j = 0; j < rows; j++){
+        for (int i = 0; i < cols; i++) {
+            std::cout << std::setw(maxwidth) << matr[i][j];
         }
-        std::cout << std::endl;
+        std::cout << "\n";
     }
 }
 
 int main(){
+    setlocale(LC_ALL,"ru");
     int len, k=1;
-    print("Введите длину стороны квадрата матрицы:");
+    print("Введите длину стороны квадрата матрицы: ");
     while (!(std::cin >> len) || (len < 1)){
-        print("Неверный размер массива:");
+        print("Неверный размер массива. Попробуйте ещё раз: ");
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
@@ -46,43 +51,53 @@ int main(){
     //Ход
     int left=0,right=len-1,up=0,down=len-1,sq=len*len;
     int maxwidth=std::to_string(sq).length()+2;
-    while (k<sq){
+    while (k<=sq){
         //слева направо
         for (int i=left;i<=right;++i){
             int j = up;
             Matrix[i][j]=k;
             k++;
-            if (k==len) break;
+            if (k==sq) break;
         }
+        up+=1;
         //сверху вниз
         for (int j=up;j<=down;++j){
             int i = right;
             Matrix[i][j]=k;
             k++;
-            if (k==len) break;
+            if (k==sq) break;
         }
+        right-=1;
         //справа налево
         for (int i=right;i>=left;--i){
             int j = down;
             Matrix[i][j]=k;
             k++;
-            if (k==len) break;
+            if (k==sq) break;
         }
+        down-=1;
         //снизу вверх
-        for (int j=down;j>up;--j){
+        for (int j=down;j>=up;--j){
             int i = left;
             Matrix[i][j]=k;
             k++;
         }
-        up+=1;
-        down-=1;
         left+=1;
-        right-=1;
     }
     //Печать матрицы
     printMatrix(Matrix, len, len, maxwidth);
+
+    //Вычисление суммы элементов побочной диагонали
+    int summa=0;
+    for(int i=0;i<len;++i){
+            int j=len-1-i;
+            summa+=Matrix[i][j];
+        }
+    print("Сумма элементов побочной диагонали: ");
+    printint(summa);
+
     //Удаление матрицы
-    for (int i = 0; i < len; ++i){
+    for (int i=0; i<len; ++i){
         delete[] Matrix[i];
     }
     delete[] Matrix;
