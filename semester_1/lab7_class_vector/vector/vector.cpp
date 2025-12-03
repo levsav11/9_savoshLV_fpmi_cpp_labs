cf5de7b17232cf79c999074ddc8990b7580fd5b8
wiline <lev.savosh11@mail.ru>
2025-12-03 20:22:08 +0300

    updated lab7

--- semester_1/lab7_class_vector/vector/vector.cpp
+++ semester_1/lab7_class_vector/vector/vector.cpp
@@ -1,2 +1,106 @@
 #include <vector.h>
 
+void Vector::Reserve(int newCapacity){
+    if (newCapacity <= capacity_) return;
+    int* oldData = data_;
+    int oldSize = size_;
+    int oldCapacity = capacity_;
+    data_ = new int[newCapacity];
+    for (int i = 0; i < oldSize; ++i){
+        data_[i] = oldData[i];
+    }
+    delete[] oldData;
+    capacity_ = newCapacity;
+}
+Vector::Vector()
+:data_(nullptr),
+ size_(0),
+ capacity_(0){};
+Vector::Vector(int size)
+:data_(new int[size]),
+ size_(size),
+ capacity_(size){
+    for (int i = 0; i < size; ++i){
+        data_[i] = 0;
+    }
+}
+Vector::Vector(std::initializer_list<int> list)
+:data_(nullptr),
+ size_(static_cast<int>(list.size())), capacity_(static_cast<int>(list.size())){
+    data_ = new int[capacity_];
+    int index = 0;
+    for (int value : list){
+        data_[index] = value;
+        ++index;
+    }
+}
+Vector& Vector::operator=(const Vector& other){
+    if (this == &other){
+        return *this;
+    }
+    delete[] data_;
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    data_ = new int[capacity_];
+    for (int i = 0; i < other.size_; ++i){
+        data_[i] = other.data_[i];
+    }
+    return *this;
+}
+int& Vector::operator[](int index){
+    return data_[index];
+ }
+const int& Vector::operator[](int index) const {
+    return data_[index];
+ }
+Vector::~Vector(){
+    delete[] data_;
+    /*data_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;*/
+}
+void Vector::Swap(Vector& other){
+    int* tmpData = data_;
+    data_ = other.data_;
+    other.data_ = tmpData;
+
+    int tmpSize = size_;
+    size_ = other.size_;
+    other.size_ = tmpSize;
+
+    int tmpCapacity = capacity_;
+    capacity_ = other.capacity_;
+    other.capacity_ = tmpCapacity;
+}
+int Vector::Size(){
+    return size_;
+}
+int Vector::Capacity(){
+    return capacity_;
+}
+void Vector::PushBack(int elem){
+    if (capacity_ == size_){
+        int newCapacity = (capacity_ == 0 ? 1 : 2 * capacity_);
+        Reserve(newCapacity);
+    }
+    data_[size_] = elem;
+    ++size_;
+}
+void Vector::PopBack(){
+    if (size_ > 0){
+    --size_;
+    }
+}
+void Vector::Clear(){
+    size_ = 0;
+}
+std::ostream& operator<<(std::ostream& os,const Vector& v){
+    os << '[';
+    for (int i = 0; i < v.size_; ++i){
+        os << v.data_[i];
+        if (i < v.size_ - 1){
+            os << ", ";
+        }
+    }
+    os << ']';
+}
--- semester_1/lab7_class_vector/vector/vector.h
+++ semester_1/lab7_class_vector/vector/vector.h
@@ -7,26 +7,24 @@ private:
     int* data_; //указатель на дин. массив
     int size_; //кол-во элементов
     int capacity_; //выделенный размер
-
-    void resize(); //изменение capacity
-
 public:
     //*конструкторы
     Vector(); //по умолчанию
     Vector(int size); //принимает размер, заполняет нулями
     Vector(std::initializer_list<int> list); //{1,2,3,4,5}
     Vector(const Vector& other); //копирования
-    Vector& operator=(Vector& other); //оператор присваивания копированием
+    Vector& operator=(const Vector& other); //оператор присваивания копированием
     ~Vector(); //деструктор
-    void Swap();
+    void Swap(Vector& other);
     //операторы индексирования const и неconst
-    int operator[](int index) const;
+    const int& operator[](int index) const;
     int& operator[](int index); //должен позволять менять
-    void Size();  //getSize
-    void Capacity();  //getCapacity
-    void PushBack(); //добавляет элемент в конец вектора + реаллокация при необх
+    int Size();  //getSize
+    int Capacity();  //getCapacity
+    void PushBack(int elem); //добавляет элемент в конец вектора + реаллокация при необх
     void PopBack(); //удаляет последний элемент, capacity не меняет
     void Clear(); //делает контейнер пустым, capacity не меняет
-    void Reserve(); //задать новую capacity (больше)
+    void Reserve(int newCapacity); //задать новую capacity (больше)
     friend std::ostream& operator<<(std::ostream& os, const Vector& v); //оператор вывода, не член класса
 };
+
