4ac7acc5fa178528b88c38f6d00f916b3a8cd529
wiline <lev.savosh11@mail.ru>
2025-12-23 22:38:18 +0300

    done rgr2 p2

--- semester_1/rgr2_vector_stl/part2/main.cpp
+++ semester_1/rgr2_vector_stl/part2/main.cpp
@@ -1,12 +1,57 @@
 #include "time_utility.h"
 #include "train.h"
-
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+void print_yellow(std::string str) {
+    std::cout << "\033[1;33m" << str << "\033[0m";
+}
+
+std::vector<Train> read_trains(std::ifstream& in){
+    std::string line;
+    std::vector<Train> result;
+    Train train;
+    while(getline(in, line) && !line.empty()) {
+        int start = line.find_first_not_of(' ', line.find_first_of(':', 0) + 1);
+        int end = line.find_first_of(';', start);
+        std::string ID = line.substr(start, end - start);
+        train.setId(std::stoi(ID));
+
+        start = line.find_first_not_of(' ', line.find_first_of(':', end) + 1);
+        end = line.find_first_of(';', start);
+        std::string type = line.substr(start, end - start);
+        train.setType(train_type_from_int(std::stoi(type)));
+
+        start = line.find_first_not_of(' ', line.find_first_of(':', end) + 1);
+        end = line.find_first_of(';', start);
+        std::string destination = line.substr(start, end - start);
+        train.setDestination(destination);
+
+        start = line.find_first_not_of(' ', line.find_first_of(':', end) + 1);
+        end = line.find_first_of(';', start);
+        std::string time = line.substr(start, end - start);
+        int hours = std::stoi(time.substr(0, 2));
+        int minutes = std::stoi(time.substr(3, 2));
+        train.setDispatchTime(SetTime(hours, minutes));
+
+        start = line.find_first_not_of(' ', line.find_first_of(':', end) + 1);
+        end = line.find_first_of(';', start);
+        time = line.substr(start, end - start);
+        hours = std::stoi(time.substr(0, 2));
+        minutes = std::stoi(time.substr(3, 2));
+        train.setTravellingTime(SetTime(hours, minutes));
+
+        result.push_back(train);
+    }
+    return result;
+}
 
 int main() {
 
     using namespace time_utility;
-
+/*
     std::random_device rd;
     std::mt19937 generator(rd());
 
@@ -27,8 +72,45 @@ int main() {
     
     std::cout << "t1 < t2: " << std::boolalpha << (t1 < t2) <<'\n';
     std::cout << "t2 < t1: " << std::boolalpha << (t2 < t1) << '\n';
+*/
 
+    std::ifstream in("trains.txt");
+    std::vector<Train> trains = read_trains(in);
+    std::sort(trains.begin(), trains.end());
+    
+    /*std::cout << "\nTrains:\n";
+    for (size_t i = 0; i < trains.size(); ++i){
+        std::cout  << trains[i];
+    }*/
 
+    print_yellow("Trains in range 12-14 hours:\n");
+    for (size_t i = 0; i < trains.size(); ++i){
+        if (trains[i].getDispatchTime() >= SetTime(12, 0) && trains[i].getDispatchTime() <= SetTime(14, 0)){
+            std::cout << trains[i];
+        }
+    }
 
+    print_yellow("\nAll trains to Hrodna:\n");
+    for (size_t i = 0; i < trains.size(); ++i){
+        if (trains[i].getDestination() == "Hrodna"){
+            std::cout << trains[i];
+        }
+    }
+
+    print_yellow("\nPassenger trains to Hrodna:\n");
+    for (size_t i = 0; i < trains.size(); ++i){
+        if (trains[i].getDestination() == "Hrodna" && trains[i].getType() == TrainType::PASSENGER){
+            std::cout << trains[i];
+        }
+    }
+
+    print_yellow("\nFastest train to Hrodna:\n");
+    int fastest_train_index = 0;
+    for (size_t i = 0; i < trains.size(); ++i){
+        if (trains[i].getDestination() == "Hrodna" && trains[i].getTravellingTime() < trains[fastest_train_index].getTravellingTime()){
+            fastest_train_index = i;
+        }
+    }
+    std::cout << trains[fastest_train_index];
     return EXIT_SUCCESS;
 }
--- semester_1/rgr2_vector_stl/part2/train.h
+++ semester_1/rgr2_vector_stl/part2/train.h
@@ -2,7 +2,7 @@
 
 #include <string>
 #include "time_utility.h"
-
+#include <iostream>
 using TrainId = size_t;
 using namespace time_utility;
 
@@ -14,12 +14,99 @@ enum class TrainType {
 
 class Train {
 private:
-    TrainId id_;
-    TrainType type_;
-    std::string destination_;
-    std::time_t dispatch_time_;
-    std::time_t travelling_time_;
+    TrainId id_ = 0;
+    TrainType type_ = TrainType::PASSENGER;
+    std::string destination_ = "Unknown";
+    std::time_t dispatch_time_ = SetTime(0, 0);
+    std::time_t travelling_time_ = SetTime(1, 0);
 
 public:
-    // your code goes here
+    Train(){};
+
+    Train(TrainId id, TrainType type, const std::string& destination, std::time_t dispatch_time, std::time_t travelling_time)
+    :id_(id), 
+     type_(type), 
+     destination_(destination), 
+     dispatch_time_(dispatch_time), 
+     travelling_time_(travelling_time){};
+
+    TrainId getId() const{
+        return id_;
+    }
+    TrainType getType() const{
+        return type_;
+    }
+    std::string getDestination() const{
+        return destination_;
+    }
+    std::time_t getDispatchTime() const{
+        return dispatch_time_;
+    }
+    std::time_t getTravellingTime() const{
+        return travelling_time_;
+    }
+
+    void setId(TrainId id){
+        id_ = id;
+    }
+    void setType(TrainType type){
+        type_ = type;
+    }
+    void setDestination(const std::string& destination){
+        destination_ = destination;
+    }
+    void setDispatchTime(std::time_t dispatch_time){
+        dispatch_time_ = dispatch_time;
+    }
+    void setTravellingTime(std::time_t travelling_time){
+        travelling_time_ = travelling_time;
+    }
+    
+    bool operator<(const Train& other) const{
+        return dispatch_time_ < other.dispatch_time_;
+    }
 };
+
+std::string train_type_to_string(TrainType type){
+    switch(type){
+        case TrainType::PASSENGER:
+            return "Passenger";
+        case TrainType::FREIGHT:
+            return "Freight";
+        case TrainType::HIGH_SPEED:
+            return "High-speed";
+        case TrainType::SUBWAY:
+            return "Subway";
+        case TrainType::SPECIALIZED:
+            return "Specialized";
+    }
+    return "Unknown";
+}
+
+TrainType train_type_from_int(int type){
+    switch(type){
+        case 0:
+            return TrainType::PASSENGER;
+        case 1:
+            return TrainType::FREIGHT;
+        case 2:
+            return TrainType::HIGH_SPEED;
+        case 3:
+            return TrainType::SUBWAY;
+        case 4:
+            return TrainType::SPECIALIZED;
+    }
+    throw std::invalid_argument("Wrong type of train!");
+}
+
+std::ostream& operator<<(std::ostream& os, const Train& t){
+    os << "ID: " << t.getId() << '\n' 
+    << "Type: " << train_type_to_string(t.getType()) << '\n'
+    << "Destination: " << t.getDestination() << '\n'
+    << "Dispatch time: ";
+    PrintTime(t.getDispatchTime()); 
+    os << "Travelling time: ";
+    PrintTime(t.getTravellingTime());
+    os << '\n';
+    return os;
+}
