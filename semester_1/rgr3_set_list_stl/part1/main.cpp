a22543e7da850ae7b4730dd7c722bf754cbdf8aa
wiline <lev.savosh11@mail.ru>
2025-12-23 23:16:41 +0300

    done rgr3 p1

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,36 @@
+#include <iostream>
+#include <set>
 
-
+void print_set(std::set<int>& numbers){
+    for (int i : numbers){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main() {
-
-    // your code goes here
-
+    int N;
+    std::cout << "\nEnter N: ";
+    if(!(std::cin >> N)){
+        std::cerr << "Input error" << std::endl;
+        return 1;
+    }
+    std::set<int> numbers;
+    for (int i = 2; i < N; ++i){
+        numbers.insert(i);
+    }
+    for (int i = 2; i < numbers.size(); ++i){
+        if (numbers.find(i) == numbers.end()){
+            continue;
+        }
+        int max = *numbers.rbegin();
+        int multiplier = 2;
+        while (max >= multiplier * i){
+            numbers.erase(multiplier * i);
+            ++multiplier;
+        }
+    }
+    std::cout << "\nPrime numbers: \n";
+    print_set(numbers);
     return 0;
 }
